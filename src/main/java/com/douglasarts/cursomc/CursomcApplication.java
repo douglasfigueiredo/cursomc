61183caea1fd63f77eb046f3fa3c1e830b79b9c9
Douglas Figueiredo <douglasarts@gmail.com>
2020-07-28 19:51:09 -0300

    Criando operação de instanciação

--- src/main/java/com/douglasarts/cursomc/CursomcApplication.java
+++ src/main/java/com/douglasarts/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.douglasarts.cursomc;
 
+import java.util.Arrays;
+
+import com.douglasarts.cursomc.domain.Categoria;
+import com.douglasarts.cursomc.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public static void main(final String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
--- src/main/java/com/douglasarts/cursomc/services/CategoriaService.java
+++ src/main/java/com/douglasarts/cursomc/services/CategoriaService.java
@@ -3,7 +3,7 @@ package com.douglasarts.cursomc.services;
 import java.util.Optional;
 
 import com.douglasarts.cursomc.domain.Categoria;
-import com.douglasarts.cursomc.repositories.CaretoriaRepository;
+import com.douglasarts.cursomc.repositories.CategoriaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class CategoriaService {
 
   @Autowired
-  private CaretoriaRepository repo;
+  private CategoriaRepository repo;
 
   public Categoria buscar(Integer id) {
     Optional<Categoria> obj = repo.findById(id);
